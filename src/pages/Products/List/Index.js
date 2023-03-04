01f5e665ce077c79dab083f95149d028e1a11a4e
Octavio Martinez <omartinez1618@gmail.com>
2023-03-04 00:35:21 -0400

    maquetado de la lista de productos 1

--- src/pages/Products/List/Index.js
+++ src/pages/Products/List/Index.js
@@ -1,4 +1,11 @@
-import { Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
+import {
+    Box,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow
+} from "@mui/material";
 import { useState } from "react";
 
 
@@ -6,33 +13,35 @@ export default function Index(){
     const [dense, setDense] = useState(false)
     const [rows, setRows] = useState([{sku:2}])
 
-    return <TableContainer>
-        <Table size={ dense ? 'small' : 'medium' }>
-            <TableBody>
-                { rows.map( row => <TableRow>
-                    <TableCell>
-                        { row.sku }
-                    </TableCell>
-                    <TableCell>
-                        { row.description }
-                    </TableCell>
-                    <TableCell>
-                        { row.mark }
-                    </TableCell>
-                    <TableCell>
-                        { row.price }
-                    </TableCell>
-                    <TableCell>
-                        { row.stock }
-                    </TableCell>
-                    <TableCell>
-                        { row.status }
-                    </TableCell>
-                    <TableCell>
-                        options
-                    </TableCell>
-                </TableRow>)}
-            </TableBody>
-        </Table>
-    </TableContainer>
+    return <Box>
+        <TableContainer data-testid='table-product-list'>
+            <Table size={ dense ? 'small' : 'medium' }>
+                <TableBody data-testid='table-content'>
+                    { rows.map( row => <TableRow>
+                        <TableCell>
+                            { row.sku }
+                        </TableCell>
+                        <TableCell>
+                            { row.description }
+                        </TableCell>
+                        <TableCell>
+                            { row.mark }
+                        </TableCell>
+                        <TableCell>
+                            { row.price }
+                        </TableCell>
+                        <TableCell>
+                            { row.stock }
+                        </TableCell>
+                        <TableCell>
+                            { row.status }
+                        </TableCell>
+                        <TableCell>
+                            options
+                        </TableCell>
+                    </TableRow>)}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Box>
 }
