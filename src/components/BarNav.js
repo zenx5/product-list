6eb42c449154c2ec857ef7ffb41f408239098745
Octavio Martinez <omartinez1618@gmail.com>
2023-03-07 01:10:33 -0400

    componente barnav

--- src/components/BarNav.js
+++ src/components/BarNav.js
@@ -1,4 +1,18 @@
+import { Grid, Typography, ButtonBase, Avatar, IconButton } from '@mui/material'
+import { ArrowDropDown, Menu } from '@mui/icons-material'
+
 export default function BarNav() {
 
-    return ''
+    return <Grid container sx={{ backgroundColor: 'primary.main', p:3 }} data-testid='barnav'>
+        <Grid item xs={6}>
+            <Typography variant='h5' color='#fff' sx={{ fontWeight:'bold' }}> <IconButton><Menu sx={{ color:'#fff' }}/></IconButton> MeuErpOnline</Typography>
+        </Grid>
+        <Grid item xs={6} sx={{ display:'flex', justifyContent:'flex-end' }}>
+            <ButtonBase>
+                <Avatar />
+                <Typography sx={{ mx:2, color:'#fff', fontWeight:'bold' }}>omartinez@gmail.com</Typography>
+                <ArrowDropDown sx={{ color:'#fff' }}/>
+            </ButtonBase>
+        </Grid>
+    </Grid>
 }
