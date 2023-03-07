67005e785b4da21cf17ee50a93134ef3e606de49
Octavio Martinez <omartinez1618@gmail.com>
2023-03-07 01:13:21 -0400

    i18n agregado y algunos estilos

---
+++ src/locales/i18n.js
@@ -0,0 +1,30 @@
+import i18n, { use } from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import enLocales from './langs/en'
+import esLocales from './langs/es'
+import ptLocales from './langs/pt'
+
+import { defaultLang } from './config-lang';
+
+let lng = defaultLang.value
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      pt: {translation: ptLocales},
+      en: {translation: enLocales},
+      es: {translation: esLocales},
+    },
+    lng,
+    fallbackLng: lng,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+
+export default i18n
