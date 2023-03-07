f01d839d8561d837d2531302d23f36b3ee3deb4f
Octavio Martinez <omartinez1618@gmail.com>
2023-03-07 01:08:29 -0400

    limpiando codigo, i18n ajuste

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// import './locales/i18n';
 import { Routes, Route } from 'react-router-dom';
-import { Main, ProductList } from './pages';
+import { ProductList } from './pages';
 
 export default function AppRoutes() {
   return <>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/system';
+import theme from './theme/theme';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './locales/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </I18nextProvider>
   </React.StrictMode>
 );
 
