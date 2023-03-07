3ab1e87d02ffda5c327587d8e11c63a6a379fdfe
Octavio Martinez <omartinez1618@gmail.com>
2023-03-07 01:09:59 -0400

    estilos y ajustes breadcrumbs

--- src/components/CustomBreadcrumbs.js
+++ src/components/CustomBreadcrumbs.js
@@ -1,14 +1,28 @@
-import { Link, Breadcrumbs } from "@mui/material"
+import { Business } from "@mui/icons-material"
+import { Box, Link, Breadcrumbs, IconButton } from "@mui/material"
 
-export default function CustomBreadcrumbs({ items = [], separator = '>', onClick }){
-    return <Breadcrumbs separator={separator}>
-        { items.map(
-            (item, index) =>
-            <Link
-                underline="hover"
-                key={`breadcrumb-${index}`}
-                href={item.href}
-                onClick={()=>onClick && onClick(item)}
-            > { item.label } </Link>) }
-    </Breadcrumbs>
+export default function CustomBreadcrumbs({ items = [], separator = '>', onClick, ...other }){
+    return <Box sx={{ ...other.sx, display:'flex', flexDirection:'row' }} {...other}>
+        <IconButton>
+            <Business />
+        </IconButton>
+        <Breadcrumbs sx={{ display:'inline-flex' }} separator={separator}>
+            { items.map(
+                (item, index) => (index===items.length-1) ?
+                <Link
+                    underline="hover"
+                    key={`breadcrumb-${index}`}
+                    href={item.href}
+                    onClick={()=>onClick && onClick(item)}
+                > { item.label } </Link>
+                : <Link
+                    underline="hover"
+                    key={`breadcrumb-${index}`}
+                    href={item.href}
+                    color='#6d6d6d'
+                    onClick={()=>onClick && onClick(item)}
+                > { item.label } </Link>
+            )}
+        </Breadcrumbs>
+    </Box>
 }
